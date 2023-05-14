63dea4223d2a31ae3bf3bb2061a19a3ae535c59e
vrglhuhu <cheanbvergel@gmail.com>
2023-05-14 16:14:48 +0800

    The Psuedocode of my program.

--- television_status_indicator.py
+++ television_status_indicator.py
@@ -1,2 +1,24 @@
 # Vergel, Chean Bernard Villanueva
 # Televison Status Indicator
+
+# Make a TV Class
+     # Initialize TV attributes
+    # Turn on the TV
+    # Turn off the TV
+    # Get the Channel of the TV
+    # Set the channel of the TV
+    # Get the Volume of the TV
+    # Set the volume of the TV
+    # Increases the channel number by 1, if it's not already at 100.
+    # Decrease the channel number by 1, if it's not already at 1.
+    # Increase the volume level by 1, if it's not already at 7.
+    # Decrease the volume level by 1, if it's not already at 1.
+    # Print the current status of the TV
+
+# TestTV Class
+     # Initialize two TV objects
+    # Turn on TV1 and set the channel and volume
+        # Turn on TV2 and set the channel and volume
+        # Print the status of both TVs
+# Create an instance of the test driver and run the test
+
